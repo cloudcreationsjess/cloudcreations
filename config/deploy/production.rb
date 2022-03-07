e13a66524ca3c05c14fca53a1b36a7db90a714cc
cloudcreationsjess <jess@cloudcreations.ca>
2022-03-06 20:55:27 -0600

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
-set :deploy_to, -> { "/home/[YOUR USERNAME]/www/[YOUR DOMAIN]" }
-set :tmp_dir, "/home/[YOUR USERNAME]/www/[YOUR DOMAIN]/tmp"
+set :deploy_to, -> { "/home/u1140-q2nxlfcuckwc/www/cloudcreations.ca" }
+set :tmp_dir, "/home/u1140-q2nxlfcuckwc/www/cloudcreations.ca/tmp"
 set :keep_releases, 2
 
 #deploy certain branch: cap staging deploy BRANCH=branch_name
@@ -15,7 +15,7 @@ set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Extended Server Syntax
 # ======================
-server '[YOUR DOMAIN]', user: '[YOUR USERNAME]', port: 18765, password: 'ssh.St34lth!', roles: %w{web app db}
+server 'cloudcreations.ca', user: 'u1140-q2nxlfcuckwc', port: 18765, password: 'ssh.C!0ud', roles: %w{web app db}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -31,4 +31,4 @@ fetch(:default_env).merge!(wp_env: :staging)
 
 SSHKit.config.command_map[:bash] = "/usr/bin"
 #SSHKit.config.command_map[:composer] = "php -d memory_limit=512M -d allow_url_fopen=1 -d suhosin.executor.include.whitelist=phar /var/www/vhosts/ql2j-whfn.accessdomain.com/composer.phar"
-SSHKit.config.command_map[:composer] = "/usr/bin/php /home/[YOUR USERNAME]/composer.phar"
+SSHKit.config.command_map[:composer] = "/usr/bin/php /home/u1140-q2nxlfcuckwc/composer.phar"
